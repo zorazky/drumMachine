18ba26e87ece385e365b6efdbb35aa3d6b3f80c9
zorazky <oswaldo.vazquez@gmail.com>
2020-05-15 10:58:17 -0500

    passing the sounds on props

--- drummachine/src/App.js
+++ drummachine/src/App.js
@@ -1,31 +1,82 @@
 import React from 'react';
 import './App.css';
 
+
+
 class App extends React.Component {
-  
-  state = {
-    keys: ['Q','W','E','A','S','D','Z','X','C']
-  }
 
   render() {
-    const { keys } = this.state;
+    const keys = ['Q','W','E','A','S','D','Z','X','C'];
+    const sounds = [
+      {
+        key: 'Q',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+      },
+      {
+        key: 'W',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+      },
+      {
+        key: 'E',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+      },
+      {
+        key: 'A',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+      },
+      {
+        key: 'S',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+      },
+      {
+        key: 'D',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+      },
+      {
+        key: 'Z',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+      },
+      {
+        key: 'X',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+      },
+      {
+        key: 'C',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+      },
+    ];
+    
     return (
-      <div id="drum-machine" className="container">
-        <div id="display" className="display">
-          {keys.map((key, idx) => (
-            <Box text={key} key={idx} />
-          ))}
-        </div>
-      </div>
+      <Drum sounds={sounds}/>
       )
   }
   
 }
 
-const Box = (props) => (
-  <div className="box">
-    {props.text}
+const Drum = (props) => (
+  <div id="drum-machine" className="container">
+    <div id="display" className="display">
+      {props.sounds.map((sound, idx) => (
+        <Box text={sound.key} key={idx} audio={sound.mp3}/>
+      ))}
+    </div>
   </div>
 )
+
+// const Box = (props) => (
+//   <div className="box">
+//     {props.text}
+//   </div>
+// )
+class Box extends React.Component {
+  render() {
+    return (
+      <div className="box">
+        {this.props.text}
+        <audio />
+      </div>
+    )
+  }
+}
 
 export default App;
